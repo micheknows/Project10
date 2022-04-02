28246afa2794894c4c0cc6e47e86bcc26776d5c7
Michelle Rogers <74469651+micheknows@users.noreply.github.com>
2022-04-02 13:06:14 -0600

    Working for console application

---
+++ Lesson10.Dal/InvoiceAdapter.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data.Common;
+
+namespace Lesson10.Dal
+{
+    public class InvoiceAdapter
+    {
+        private string _connectionString = @"Data Source= Chinook_Sqlite_AutoIncrementPKs.sqlite; datetimeformat=CurrentCulture;";
+
+        public List<Invoice> GetAll()
+        {
+            // Declare the return type
+            List<Invoice> returnValue = new List<Invoice>();
+            // Create a connection to SQL lite. Wrap in a using statement for safety
+            using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+            {
+                // Create the commamd
+                SQLiteCommand command = connection.CreateCommand();
+                // Pass the CommandText to the command
+                command.CommandText = "SELECT InvoiceId, CustomerId, InvoiceDate, Total FROM Invoice";
+                // Open the database connection
+                connection.Open();
+                // Execute a command and return back a reader
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    // Call a method to retrieve the results
+                    Invoice customer = GetFromReader(reader);
+                    // add the instance to the return list
+                    returnValue.Add(customer);
+                }
+                // return back the results
+                return returnValue;
+            }
+        }
+
+        public List<Invoice> GetByCustomerId(int customerId)
+        {
+            List<Invoice> returnValue = new List<Invoice>();
+            using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+            {
+                // Create the command
+                SQLiteCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT InvoiceId, CustomerId, InvoiceDate, Total FROM Invoice WHERE CustomerId = " + customerId.ToString();
+              connection.Open();
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Invoice invoice = GetFromReader(reader);
+                    returnValue.Add(invoice);
+                }
+
+                return returnValue;
+            }
+        }
+
+        public Invoice GetById(int invoiceId)
+        {
+            Invoice returnValue = null;
+            using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+            {
+                // Create the command
+                SQLiteCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT InvoiceId, CustomerId, Amount, InvoiceDate FROM Invoice WHERE InvoiceId = " + invoiceId.ToString();
+              connection.Open();
+                SQLiteDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    returnValue = GetFromReader(reader);
+                }
+
+                return returnValue;
+            }
+        }
+
+
+        private Invoice GetFromReader(DbDataReader reader)
+        {
+            // Create a new instance of the customer class
+            Invoice invoice = new Invoice();
+            // Copy the data that you retrieve from the database into the class
+            invoice.CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId"));
+            invoice.InvoiceId = reader.GetInt32(reader.GetOrdinal("InvoiceId"));
+            invoice.Total = reader.GetDecimal(reader.GetOrdinal("Total"));
+            invoice.InvoiceDate = reader.GetDateTime(reader.GetOrdinal("InvoiceDate"));
+            return invoice;
+        }
+
+        public bool InsertInvoice(Invoice invoice)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+            {
+                SQLiteCommand command = connection.CreateCommand();
+                command.CommandText = "INSERT INTO Invoice (CustomerId, InvoiceDate, Total) VALUES ('" + invoice.CustomerId + "','" + invoice.InvoiceDate + "','" + invoice.Total + "'); ";
+                connection.Open();
+                int rows = command.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool UpdateInvoice(Invoice invoice)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+            {
+                SQLiteCommand command = connection.CreateCommand();
+                command.CommandText = "UPDATE Invoice SET Total = " + invoice.Total + ", InvoiceDate = '" + invoice.InvoiceDate + "' WHERE InvoiceId = " + invoice.InvoiceId;
+                connection.Open();
+                int rows = command.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool DeleteInvoice(int invoiceId)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+            {
+                SQLiteCommand command = connection.CreateCommand();
+                command.CommandText = "DELETE FROM Invoice WHERE InvoiceId = " + invoiceId;
+                connection.Open();
+                int rows = command.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Lesson10.Test/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson10.Dal;
+
+namespace Lesson10.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the customer's first name:");
+            String fname = Console.ReadLine();
+            Console.WriteLine("Please enter the customer's last name:");
+            String lname = Console.ReadLine();
+            Console.WriteLine("Please enter the customer's country:");
+            String country = Console.ReadLine();
+            Console.WriteLine("Please enter the customer's email:");
+            String email = Console.ReadLine();
+
+            CustomerAdapter adapter = new CustomerAdapter();
+            Customer customer = new Customer();
+            customer.FirstName = fname;
+            customer.LastName = lname;
+            customer.Country = country;
+            customer.Email = email;
+            adapter.InsertCustomer(customer);
+            Console.WriteLine("Customer Inserted.  Press <enter> to see all current customers.");
+            Console.ReadLine();
+            List<Customer> customers = adapter.GetAll();
+
+            foreach (Customer c in customers)
+            {
+                Console.WriteLine(c.CustomerId + ":" + c.FirstName + " " + c.LastName + ":" + c.Country + ":" + c.Email);
+            }
+            Console.ReadLine();
+
+
+            
+            
+            Console.WriteLine("Please enter the customer's id for the invoice:");
+            int cid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the invoice date:");
+            DateTime invdate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the invoice total:");
+            Decimal total = Decimal.Parse(Console.ReadLine());
+            InvoiceAdapter invadapter = new InvoiceAdapter();
+            Invoice invoice = new Invoice();
+            invoice.CustomerId = cid;
+            invoice.InvoiceDate = invdate;
+            invoice.Total = total;
+            invadapter.InsertInvoice(invoice);
+            Console.WriteLine("Invoice Inserted.  Press <enter> to see all current invoices.");
+            List<Invoice> invoices = invadapter.GetAll();
+            Console.ReadLine();
+
+            foreach (Invoice i in invoices)
+            {
+                Console.WriteLine(i.InvoiceId + "  :  " + i.CustomerId + "  :  " + i.InvoiceDate + "  :  $" + i.Total);
+            }
+            Console.ReadLine();
+        }
+    }
+}
